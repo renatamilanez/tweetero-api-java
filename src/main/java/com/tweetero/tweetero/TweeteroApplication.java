c0ab5a02041853ea988064d4d2fa0428913d1a52
Renata Milanez <renatamilanez.dev@gmail.com>
2023-04-10 16:28:22 -0300

    first commit

---
+++ src/main/java/com/tweetero/tweetero/TweeteroApplication.java
@@ -0,0 +1,13 @@
+package com.tweetero.tweetero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeteroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeteroApplication.class, args);
+	}
+
+}
